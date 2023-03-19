e763a3bf572218bc6f9dbd65dc60c5f2231854c7
tapiaht <hiver.tapia@uab.edu.bo>
2023-03-19 00:32:47 -0400

    fin seccion4

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.sass']
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
   anio: number = new Date().getFullYear();
-  constructor() { }
+  constructor(public _service:InfoPaginaService) { }
 
   ngOnInit(): void {
   }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(public _service:InfoPaginaService ) { }
   ngOnInit(): void {
   }
 
